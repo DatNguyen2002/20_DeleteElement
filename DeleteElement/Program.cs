13c91ddca2bba98ef3042361da6a914975fb5f33
DatNguyen2002 <90488040+DatNguyen2002@users.noreply.github.com>
2023-06-08 15:59:34 +0700

    Update Program.cs

--- DeleteElement/Program.cs
+++ DeleteElement/Program.cs
@@ -10,6 +10,34 @@ namespace DeleteElement
     {
         static void Main(string[] args)
         {
+
+            int[] array = { 1, 3, 4, 6, 8, 3, 6 };
+
+            Console.WriteLine("Nhap so can xoa: ");
+            int value = int.Parse(Console.ReadLine());
+
+            int count = 0; // Số phần tử bị xóa
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == value)
+                {
+                    count++;
+                }
+                else
+                {
+                    array[i - count] = array[i];
+                }
+            }
+
+            Array.Resize(ref array, array.Length - count);
+
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadKey();
         }
     }
 }
